1cdbced61f09fdf3f2a837d3e64bb8302d875c41
Piyush Thaware <Piyushbthaware@gmail.com>
2024-09-22 23:52:25 +0530

    Added

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    eslint: {
+        ignoreDuringBuilds: true, // This will skip ESLint checks during production builds
+    },
+};
 
 export default nextConfig;
